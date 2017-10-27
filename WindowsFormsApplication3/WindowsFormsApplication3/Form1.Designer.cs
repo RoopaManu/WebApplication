d9c255c7274b721517a9cd1deed5f8ff1bd8822e
RoopaManu <30858471+RoopaManu@users.noreply.github.com>
2017-10-27 13:49:09 +0530

    Initial commit
    First Web

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblheading = new System.Windows.Forms.Label();
+            this.btnenter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblheading
+            // 
+            this.lblheading.AutoSize = true;
+            this.lblheading.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblheading.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblheading.Location = new System.Drawing.Point(226, 67);
+            this.lblheading.Name = "lblheading";
+            this.lblheading.Size = new System.Drawing.Size(156, 20);
+            this.lblheading.TabIndex = 0;
+            this.lblheading.Text = "SAR Techno Solutions";
+            this.lblheading.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnenter
+            // 
+            this.btnenter.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenter.Location = new System.Drawing.Point(258, 123);
+            this.btnenter.Name = "btnenter";
+            this.btnenter.Size = new System.Drawing.Size(75, 23);
+            this.btnenter.TabIndex = 1;
+            this.btnenter.Text = "Enter";
+            this.btnenter.UseVisualStyleBackColor = true;
+            this.btnenter.Click += new System.EventHandler(this.btnenter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Billing System with GST";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnenter);
+            this.Controls.Add(this.lblheading);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblheading;
+        private System.Windows.Forms.Button btnenter;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.OleDb.OleDbCommand;
+using System.Exception;
+
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 ob3 =new Form3();
+            ob3.Show();
+            this.Hide();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            var conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;Data Source=C:\\Users\\Roopa\\Documents\\Buisness.accdb");
+            conn.Open();
+            OleDbCommand cmd =new OleDbCommand("SELECT (*) FROM Table1", conn);
+            conn.Open();
+            
+            
+                int i;
+                i =Convert.ToInt32(cmd.ExecuteReader());
+                if (i == 1)
+                {
+                MessageBox.Show("Success");
+                }
+               else
+                {MessageBox.Show("Invalid User Name or Password");
+               }
+            
+
+
+        }
+    }
+}
